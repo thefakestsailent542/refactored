b66fa1800bdd5bc3e139ad02d6eb0828839c9a3c
thefakestsailent542 <82805599+thefakestsailent542@users.noreply.github.com>
2025-03-19 21:51:22 +0800

    Update refactored.py

--- refactored.py
+++ refactored.py
@@ -30,3 +30,31 @@ sorted_rl_categories = ["Least Concern (LC)", "Near Threatened (NT)", "Vulnerabl
 df["RL Category Full"] = df["RL Category"].map(rl_category_mapping)
 
 st.sidebar.header("Filter Data")
+
+
+
+
+
+# filters on side
+family_filter = st.sidebar.multiselect("Select Family", df["Family"].unique())
+rl_category_filter = st.sidebar.multiselect("Select RL Category (Red List Category)", sorted_rl_categories)
+common_name_filter = st.sidebar.text_input("Search by Common Name")
+scientific_name_filter = st.sidebar.text_input("Search by Scientific Name")
+
+filtered_df = df
+
+
+
+
+
+if family_filter:
+    filtered_df = filtered_df[filtered_df["Family"].isin(family_filter)]
+if rl_category_filter:
+    selected_rl_values = [key for key, value in rl_category_mapping.items() if value in rl_category_filter]
+    filtered_df = filtered_df[filtered_df["RL Category"].isin(selected_rl_values)]
+if common_name_filter:
+    filtered_df = filtered_df[filtered_df["Common name"].str.contains(common_name_filter, case=False, na=False)]
+if scientific_name_filter:
+    filtered_df = filtered_df[filtered_df["Scientific name"].str.contains(scientific_name_filter, case=False, na=False)]
+
+
